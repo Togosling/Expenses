a511f4d489454c81f1a0c727a337628a66c8aa39
Togosling <toga.abdyldaev@gmail.com>
2023-12-07 11:40:54 +0600

    Initial Commit

---
+++ Expenses/ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesApp.swift
+//  Expenses
+//
+//  Created by Тагай Абдылдаев on 2023/12/7.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
